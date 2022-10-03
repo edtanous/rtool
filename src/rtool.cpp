a51912db0d226c4efbea0a4ccaf33bf6009adc24
Ed Tanous <edtanous@google.com>
2022-10-03 15:36:54 -0700

    Add sensor list
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- src/rtool.cpp
+++ src/rtool.cpp
@@ -9,9 +9,15 @@ int main(int argc, char** argv)
     CLI::App* sensor =
         app.add_subcommand("sensor", "Sensor related subcommands");
     // Define options
-    sensor->add_option("list", "Sensor list");
+    CLI::Option* sensor_list_opt = sensor->add_option("list", "Sensor list");
 
     CLI11_PARSE(app, argc, argv);
+
+    if(sensor_list_opt != nullptr){
+        std::cout << "doing sensor list\n";
+        return EXIT_SUCCESS;
+    }
+
 
     boost::asio::io_context ioc;
     http::Client http(ioc);
