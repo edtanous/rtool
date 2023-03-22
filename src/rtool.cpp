2b607b02ba4f696074d13ceab4b64f5d085facce
Ed Tanous <edtanous@google.com>
2023-03-21 17:45:27 -0700

    new command raw
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- src/rtool.cpp
+++ src/rtool.cpp
@@ -1,18 +1,16 @@
 #include <CLI/CLI.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/json.hpp>
+#include <boost/json/basic_parser_impl.hpp>
 #include <optional>
 
 #include "http_client.hpp"
 
-struct HostData {
-  std::string host;
-  uint16_t port;
-};
-
 // The null parser discards all the data
 class redpath_parser {
   struct handler {
+    std::string redpath;
+    bool redpath_match = true;
     constexpr static std::size_t max_object_size = std::size_t(-1);
     constexpr static std::size_t max_array_size = std::size_t(-1);
     constexpr static std::size_t max_key_size = std::size_t(-1);
@@ -22,34 +20,104 @@ class redpath_parser {
 
     bool on_document_begin(error_code&) { return true; }
     bool on_document_end(error_code&) { return true; }
-    bool on_object_begin(error_code&) { return true; }
+    bool on_object_begin(error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
+      return true;
+    }
     bool on_object_end(std::size_t, error_code&) { return true; }
-    bool on_array_begin(error_code&) { return true; }
+    bool on_array_begin(error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
+      return true;
+    }
     bool on_array_end(std::size_t, error_code&) { return true; }
-    bool on_key_part(std::string_view, std::size_t, error_code&) {
+    bool on_key_part(std::string_view key, std::size_t key_size, error_code&) {
+      std::string_view sv(redpath);
+      std::cout << "Comparing " << key << " to "
+                << sv.substr(key_size - key.size(), key.size()) << "\n";
+      std::cout << "redpath_match before is " << redpath_match << "\n";
+      bool sub_matches = sv.substr(key_size - key.size(), key.size()) == key;
+      std::cout << "Sub matches " << sub_matches << "\n";
+      redpath_match = redpath_match && sub_matches;
+      std::cout << "redpath_match is " << redpath_match << "\n";
       return true;
     }
-    bool on_key(std::string_view, std::size_t, error_code&) { return true; }
-    bool on_string_part(std::string_view, std::size_t, error_code&) {
+    bool on_key(std::string_view key, std::size_t key_size, error_code& ec) {
+      return on_key_part(key, key_size, ec);
+    }
+    bool on_string_part(std::string_view str, std::size_t, error_code&) {
+      if (redpath_match) {
+        std::cout << str;
+      }
       return true;
     }
-    bool on_string(std::string_view, std::size_t, error_code&) { return true; }
+    bool on_string(std::string_view str, std::size_t str_size, error_code& ec) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+      on_string_part(str, str_size, ec);
+
+      std::cout << "\n";
+
+      return true;
+    }
     bool on_number_part(std::string_view, error_code&) { return true; }
-    bool on_int64(std::int64_t, std::string_view, error_code&) { return true; }
-    bool on_uint64(std::uint64_t, std::string_view, error_code&) {
+    bool on_int64(std::int64_t, std::string_view, error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
       return true;
     }
-    bool on_double(double, std::string_view, error_code&) { return true; }
-    bool on_bool(bool, error_code&) { return true; }
-    bool on_null(error_code&) { return true; }
-    bool on_comment_part(std::string_view, error_code&) { return true; }
-    bool on_comment(std::string_view, error_code&) { return true; }
+    bool on_uint64(std::uint64_t, std::string_view, error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+      return true;
+    }
+    bool on_double(double, std::string_view, error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
+      return true;
+    }
+    bool on_bool(bool, error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
+      return true;
+    }
+    bool on_null(error_code&) {
+      if (!redpath_match) {
+        redpath_match = true;
+        return true;
+      }
+
+      return true;
+    }
+    bool on_comment_part(std::string_view, error_code&) { return false; }
+    bool on_comment(std::string_view, error_code&) { return false; }
   };
 
   boost::json::basic_parser<handler> p_;
 
  public:
-  redpath_parser() : p_(boost::json::parse_options()) {}
+  redpath_parser(std::string_view redpath)
+      : p_(boost::json::parse_options(), std::string(redpath)) {}
 
   ~redpath_parser() {}
 
@@ -136,28 +204,30 @@ void pretty_print(std::ostream& os, boost::json::value const& jv,
   if (indent->empty()) os << "\n";
 }
 
-static void handle_response(const std::shared_ptr<http::Client>&,
+static void handle_response(const std::string& redpath,
+                            const std::shared_ptr<http::Client>&,
                             http::Response&& res) {
   std::string_view ct = res.getHeader(boost::beast::http::field::content_type);
   if (ct != "application/json" && ct != "application/json; charset=utf-8") {
     return;
   }
   boost::system::error_code ec;
-  boost::json::value j = boost::json::parse(res.body(), ec);
+  redpath_parser p(redpath);
+  p.write(res.body().data(), res.body().size(), ec);
 
   if (ec) {
     return;
   }
-  pretty_print(std::cout, j);
 }
 
 static void getRedpath(std::string_view host, uint16_t port,
                        const std::shared_ptr<http::Client>& client,
-                       std::string_view /*redpath*/) {
+                       std::string_view redpath) {
   boost::beast::http::fields headers;
-  client->sendData(std::string(), host, port, "/redfish/v1", headers,
-                   boost::beast::http::verb::get,
-                   std::bind_front(&handle_response, client));
+  client->sendData(
+      std::string(), host, port, "/redfish/v1", headers,
+      boost::beast::http::verb::get,
+      std::bind_front(&handle_response, std::string(redpath), client));
 }
 
 int main(int argc, char** argv) {
@@ -206,10 +276,6 @@ int main(int argc, char** argv) {
       getRedpath(host, *port, http, redpath);
     }
   } else {
-    boost::beast::http::fields headers;
-    http->sendData(std::string(), host, *port, "/redfish/v1", headers,
-                   boost::beast::http::verb::get,
-                   std::bind_front(&handle_response, http));
   }
   http.reset();
   ioc.run();
